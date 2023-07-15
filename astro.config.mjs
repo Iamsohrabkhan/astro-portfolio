3aad8b4f2a422c805c43985a8f4f22e9ee2c7312
Sohrab Khan <7ohrabkhan@gmail.com>
2023-07-15 03:04:30 -0700

    removing keystatic and adding animation in blogCards

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import image from "@astrojs/image";
 
-// https://astro.build/config
-export default defineConfig({
-  markdown: {
-    drafts: true,
-  },
-  integrations: [tailwind(), react(),image({
+export default defineConfig({integrations: [tailwind(), react(),image({
     serviceEntryPoint: '@astrojs/image/sharp',
   }),]
 });
