b2f506ed62727f7a830cfa29be9ad10d73d259b1
Sohrab Khan <7ohrabkhan@gmail.com>
2023-08-03 02:43:43 -0700

    changing dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,39 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors:{
-				dark:"#20232A"
-			},
-		fontFamily:{
-			grotesk: ["'Space Grotesk', 'sans-serif'"]
-		},
-		keyframes: {
-			animatedgradient: {
-			  '0%': { backgroundPosition: '0% 50%' },
-			  '50%': { backgroundPosition: '100% 50%' },
-			  '100%': { backgroundPosition: '0% 50%' },
-			},
-		  },
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#20232A",
+      },
+      fontFamily: {
+        grotesk: ["'Space Grotesk', 'sans-serif'"],
+      },
+      keyframes: {
+        animatedgradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
 
-		animation:{			
-			"slow-fade" : "fade 2s both",	
-			"spin-slow": "spin 10s linear infinite",
-			gradient: 'animatedgradient 6s ease infinite alternate',	
-		},
-		backgroundSize: {
-			'300%': '300%',
-		  },
-		  backgroundImage: {
-			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-		  }
-		},
-
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-		require('tailwindcss-animated')
-	],
-}
+      animation: {
+        "slow-fade": "fade 2s both",
+        "spin-slow": "spin 10s linear infinite",
+        gradient: "animatedgradient 6s ease infinite alternate",
+      },
+      backgroundSize: {
+        "300%": "300%",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwindcss-animated"),
+  ],
+};
